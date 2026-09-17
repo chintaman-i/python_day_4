68f9546e02835d940ce0291bcdbc4df85aa5ea32
chintaman-i <chintamani8586@gmail.com>
2026-09-17 14:15:49 +0530

    Q2

---
+++ 2.py
@@ -0,0 +1,17 @@
+#def pi():
+   # return 3.14
+
+#print(pi())
+    #x=pi()
+#
+#print(x)
+
+#def greet(name="Student"):
+ #   print("Hello",name)
+#greet("John")
+#greet()
+
+def square(n):
+    return n*n
+
+print(square(5))
